676caca01254a0570e8deb310598cb5608443794
Tushar <tushar@weboniselab.com>
2013-12-26 19:31:28 +0530

    all the files

---
+++ assignment3/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Cloud Foundry</title>
+		<link rel="stylesheet" type="text/css" href="/assignment3/css/style.css">
+	</head>
+	<body>	
+
+<!-- globle header -->
+		<div class="main_header">
+			<div class="header_960">
+				<hgroup>
+					<a href="">FAQ'S</a>
+					<a href="">LOGIN</a>
+				</hgroup>
+				<div class="SiteSearch">
+    				<form action="" id="searchform" method="get">
+      					  <input x-webkit-speech="" autocomplete="off" type="text" id="gText" name="q" id="q" placeholder="Search.."/>
+      					  <input type="submit" id="gBtn"/>
+   				 	</form>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+<!-- logo -->
+		<div class="wrap">
+			<div class="leftside">
+				<a href="">
+					<span class="logo">
+			     		 Cloud Foundry
+					</span>
+				</a>
+			</div>
+			<div class="clear"></div>
+<!-- nav bar -->
+	 		<div class="main-nav">
+				<nav>
+					<ul>
+						<li>HOME</li>
+						<li>ABOUT US</li>
+						<li>CHAPTERS</li>
+						<li>EVENTS</li>
+						<li>WIKI</li>
+					</ul>
+				</nav>
+		    </div>
+		    <div class="clear"></div>
+<!-- main slider-->
+			<div class="slider">
+				<div class="left-content">
+					<h1>Libera World of	
+					<br />					
+					Knowledge Sharing for 
+					Developers
+					</h1>
+					<h5>	
+					Your key of an awesome knowledge cloud
+					</h5>	
+					<input type="button" class="" name="sign up" value="SIGN UP">
+				</div>
+				<div class="right-content">
+					<nav>
+						<ul>
+							<li class="right-arrow"> <a href=""></a></li>
+							<li class="first"></li>
+							<li class="second"></li>
+							<li class="third"></li>
+							<li class="left-arrow"></li>
+						</ul>
+
+					</nav>
+				</div>
+			</div>
+			<div class="clear"></div>
+<!-- main slider-->			
+
+		</div>
+	</body>	
+</html>
